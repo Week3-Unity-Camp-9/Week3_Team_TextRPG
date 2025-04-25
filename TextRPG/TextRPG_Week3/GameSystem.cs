edb44323fa1eadb5cc96fa9c813967d86dff9e76
WaffleManse <damiano1994@naver.com>
2025-04-25 16:34:36 +0900

    기능 설명 주석
    기능 설명 주석

--- TextRPG/TextRPG_Week3/GameSystem.cs
+++ TextRPG/TextRPG_Week3/GameSystem.cs
@@ -15,6 +15,8 @@ namespace TextRPG_Week3
             Quests = quests;
         }
     }
+        //public class GameData
+        //캐릭터, 상점아이템, 퀘스트리스트를 묶는 클래스
 
     public static class SaveManager
     {
@@ -25,6 +27,11 @@ namespace TextRPG_Week3
             string json = JsonConvert.SerializeObject(gameData, Formatting.Indented);
             File.WriteAllText(saveFilePath, json);
         }
+        //SaveGame함수(캐릭터클래스, 상점아이템클래스, 퀘스트리스트, 정수값을 매개변수로 받는다.)
+        //받은 정수 slot을 통해 세이브파일의 이름을 정하고
+        //새로운 게임데이터 클래스에 받아온 캐릭터, 상정아이템, 퀘스트리스트 매개변수들을 넣어 참조한다.
+        //Json변환(직렬화) 함수를 활용해서 게임데이터를 문자열로 변환하고
+        //세이브파일 이름에 변환된 문자열을 넣어 파일을 생성한다.
 
         public static (Character, ShopItem, List<Quest>) LoadGame(int slot)
         {
@@ -35,6 +42,11 @@ namespace TextRPG_Week3
 
             return (gameData.Player, gameData.Shop, gameData.Quests);
         }
+        //LoadGame함수(캐릭터, 상점아이템, 퀘스트리스트 클래스를 반환한다) (정수값을 매개변수로 받는다.)
+        //받은 정수 slot값을 통해 세이브파일 이름을 정하고
+        //세이브파일에서 문자열을 불러온다.
+        //Json변환(역직렬화) 함수를 활용하여 불러온 문자열을 게임데이터 형식에 맞게 변환
+        //불러온 값들을 각각 형태에 맞게 반환한다.
     }
 
     public static class GameSystem
@@ -78,5 +90,12 @@ namespace TextRPG_Week3
                 return -1;
             }
         }
+        //Select함수
+        //값을 입력받을때 사용되는 공통된 코드들을 모아놓은 함수로
+        //정수값을 반환하며
+        //(문자열 배열 options = 기본값 null,
+        //불값 hasExit = 기본값 true,
+        //문자열 zeroSelection = 기본값 "0.나가기",
+        //문자열 question = 기본값 "원하시는 행동을 입력해 주세요.\n>>") 의 네 가지의 매개변수를 받는다.
     }
 }
